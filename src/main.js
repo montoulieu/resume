4c4eb92c17750d0e02ce322cb2e61fe12df56709
pi-mont <pi.mont@icloud.com>
2018-10-20 17:30:15 -0600

    Moved Resume base style edits to scss file that loads after bootstrap

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import Resume from './Resume'
 import router from './router'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import './assets/sass/resume.scss'
 import './svg-triangles'
 
 Vue.use(BootstrapVue)
