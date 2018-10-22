f85a5e9c0d708716006c66b212f58fda9dfd68b7
pi-mont <pi.mont@icloud.com>
2018-10-22 10:40:55 -0600

    Misc job description text change to push up changes after yesterdays Github downtime

--- src/components/pages/Experience.vue
+++ src/components/pages/Experience.vue
@@ -31,7 +31,7 @@ export default {
     return {
       description: `
         <p>I originally started tinkering with web technologies in the 5th grade and since<br class="d-none d-md-block"> then have continously pushed myself to learn the latest and greatest in the field.</p>
-        <p>Moving to Denver opened up so many more opportunities to work with agencies and land freelance clients that I never really had back in Florida.</p>
+        <p>Moving to Denver opened up so many opportunities to work with agencies and land freelance clients that I never really had back in Florida.</p>
         <p>Because of this, the last 5 years have brought exponential growth to my coding skillset and the connections I've made make me proud to live and work here. </p>
       `,
       jobs: [
